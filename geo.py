36ca80302f39937714b334d60e37aa2c5523f234
Akeem Elewuro <115774708+AkeemElewuro@users.noreply.github.com>
2024-11-29 08:34:09 +0100

    Fix load_data function geo.py

--- geo.py
+++ geo.py
@@ -24,7 +24,7 @@ API_KEY = "AIzaSyCxMDrxcYW2Q5CMtqmm9TOlSi0iAVYhDJ0"
 def load_data():
     """Load geocoded data from a shared or public URL."""
     try:
-        return pd.read_csv(CSV_URL, encoding='ISO-8859-1', error_bad_lines=False, warn_bad_lines=True)
+        return pd.read_csv(CSV_URL, encoding='ISO-8859-1', on_bad_lines='skip')
     except Exception as e:
         st.error(f"Error loading data: {e}")
         return pd.DataFrame()
